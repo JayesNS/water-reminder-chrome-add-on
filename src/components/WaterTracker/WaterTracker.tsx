35cca5bbc5dee218e54f8c2e5ddeff84989aac68
jszkarlat <jakub.szkarlat@comarch.com>
2022-12-29 14:21:59 +0100

    Added clamp() utility function

--- src/components/WaterTracker/WaterTracker.tsx
+++ src/components/WaterTracker/WaterTracker.tsx
@@ -1,6 +1,6 @@
 import {useCallback, useRef, useMemo, MutableRefObject} from 'react';
 import {useFillController} from '../../hooks';
-import { PercentFormatter } from '../../utils/formatters';
+import {PercentFormatter} from '../../utils/formatters';
 
 import './WaterTracker.css';
 
@@ -31,7 +31,7 @@ const WaterTracker = ({cupSize, value, onClick}: WaterTrackerProps) => {
                     <rect ref={fillRef} className="WaterTracker__content" id="Content" x="49" y="10" width="101" height="142" fill="url(#paint0_linear_211_170)"/>
                     <path id="Shadow" d="M155.887 9.10731C155.948 7.96223 155.036 7 153.89 7H127.029L99.894 8.44025C98.8321 8.49661 98 9.37406 98 10.4374V153C98 154.105 98.8954 155 100 155H126H146.152C147.215 155 148.092 154.169 148.149 153.107L155.887 9.10731Z" fill="#023E8A" fillOpacity="0.1"/>
                     <path id="Outline" d="M153 40C219.714 2.00001 199 118.069 147.786 118.069M99.5714 8.58621L48.1822 6.10535C47.0094 6.04873 46.0404 7.00978 46.0873 8.18296L51.9232 154.08C51.9661 155.153 52.8481 156 53.9216 156H145.317C146.391 156 147.273 155.152 147.316 154.079L153.057 8.18159C153.103 7.0089 152.134 6.04869 150.962 6.10528L99.5714 8.58621Z" stroke="#EDEDE9" strokeOpacity="0.6" strokeWidth="12" strokeLinejoin="round"/>
-                    <rect ref={containerRef} id="Click_area" x="49" y="10" width="101" height="142" fill="transparent"></rect>
+                    <rect ref={containerRef} id="Click_area" x="49" y="10" width="101" height="142" fill="transparent" />
                     <g id="Border">
                         <path id="Body_3" d="M46.0873 8.18296C46.0404 7.00978 47.0094 6.04873 48.1822 6.10535L99.5714 8.58621L150.962 6.10528C152.134 6.04869 153.103 7.0089 153.057 8.18159L147.316 154.079C147.273 155.152 146.391 156 145.317 156H99H53.9216C52.8481 156 51.9661 155.153 51.9232 154.08L46.0873 8.18296Z" stroke="#8BC4D3" strokeWidth="8" strokeLinejoin="round"/>
                         <path id="Handle" d="M153 40C219.714 2.00001 199 118.069 147.786 118.069" stroke="#8BC4D3" strokeWidth="8" strokeLinejoin="round"/>
--- src/utils/MathUtils.ts
+++ src/utils/MathUtils.ts
@@ -8,6 +8,9 @@ const MathUtils = {
         }
         return Math.round(num / precision) * precision;
     },
+    clamp(value: number, min: number, max: number): number {
+        return Math.min(Math.max(value, min), max);
+    },
 };
 
 export default MathUtils;
